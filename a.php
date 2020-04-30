1207c71f7b7105ec9a891befe60e9aebd3b30833
Vivek Ranjan Pandey <v.pandey@salubrioustech.com>
2020-04-30 09:45:19 +0530

    dfdf

--- a.php
+++ a.php
@@ -1,20 +1,9 @@
 <?php
 
- if($a>0){
- 	$a = 4+5;
-  $p = 90 + 90;
-  // branch mearging
-  // rummy
-  //welcome to debopps
-  //this is dog-2
- 	$b = 10-2;
- }
- // this is a dog
- // this is a testing rrepo
+ $h = 90 + 90;
 
-//this is testing line-1
-// good to work with college
- if($g>90) {
- 	$FG = 6+20;
- }
-// this is testing line -2
+ $a = 98 + 9;
+
+ $g = 4 + 4;
+
+ ?>
